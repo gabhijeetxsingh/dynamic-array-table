8b53cc28c14c7b3dd0b27dfdcab840f97f4d2253
abhijeet singh <honeybest116@gmail.com>
2018-07-05 17:44:14 +0530

    search working accrding to single word

--- modal/list.js
+++ modal/list.js
@@ -6,11 +6,11 @@ function fetchdata(datas) {
     return new Promise(function (resolve, reject) {
 
         console.log("datas in promise"+datas);
-        request(`https://jsonplaceholder.typicode.com/posts?userId=${datas}`, function (error, response, body) {
+        request(`https://jsonplaceholder.typicode.com/posts?userId=3`, function (error, response, body) {
             console.log('error:', error); // Print the error if one occurred
             console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
             
-            console.log('body:', body); // Print the HTML for the Google homepage.
+            console.log(body); // Print the HTML for the Google homepage.
 
 
             if (error) {    
@@ -19,7 +19,9 @@ function fetchdata(datas) {
 
                 reject(error);
             } else {
-                resolve(body);
+                resolve(JSON.parse(body));
+
+
                 //console.log(rows);
             }
 
--- routes/index.js
+++ routes/index.js
@@ -4,16 +4,27 @@ var listdata = require('../modal/list.js');
 
 /* GET home page. */
 let searchval;
+
 router.get('/', function (req, res, next) {
 
   searchval = req.query.searchstring;
   listdata.fetchdata(searchval)
   .then(function (data) {
-
+    let fdata =  data.filter(function(values) {
+      console.log(values);
+       if(values.body.includes(searchval)==true)
+       {
+        console.log("data aa raha hai"+values.body.includes(searchval));
+        return values;
+       }
      
+     
+    });
+    console.log('##########################################################');
+    console.log(fdata);
       res.render('index', {
         title: 'Users',
-        rows: data
+        rows: JSON.stringify(fdata)
       });
 
   })
